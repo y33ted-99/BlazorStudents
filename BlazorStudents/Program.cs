3560eef5c0dffa4d3d4f2cc45b9e9ae6ebd701a3
Brian Mak <brianmak.blue@gmail.com>
2024-11-25 16:46:55 -0800

    yes

---
+++ BlazorStudents/Program.cs
@@ -0,0 +1,41 @@
+using BlazorStudents.Client.Pages;
+using BlazorStudents.Components;
+using System.Net.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddScoped<HttpClient>(sp =>
+{
+    // Provide a base address for HttpClient
+    return new HttpClient { BaseAddress = new Uri("https://apipool.azurewebsites.net/") };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorStudents.Client._Imports).Assembly);
+
+app.Run();
